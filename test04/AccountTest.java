abe5a73027d3cb6d5485990938eefccfe50184dc
anwkrwjd <80314935+anwkrwjd@users.noreply.github.com>
2021-07-13 23:56:07 +0900

    Add files via upload

---
+++ test04/AccountTest.java
@@ -0,0 +1,82 @@
+package com.lgcns.test04;
+class account
+{
+   private String account;
+   private int balance;
+   private double interestRate; 
+
+   public account() 
+   {
+	   
+   }
+
+   public account (String account, int balance, double interestRate)
+   {
+      this.account = account;
+      this.balance = balance;
+      this.interestRate = interestRate;
+   }
+
+   public void setAccount ( String account ) 
+   {
+      this.account = account;
+   }
+
+   public String getAccount () 
+   {
+      return account;
+   }
+
+   public void setBalance(int balance) 
+   {
+	   this.balance=balance;
+   }
+
+   public int getBalance() 
+   {
+	   return balance;
+   }
+
+   public void setInterestRate(double interestRate) 
+   {
+	   this.interestRate=interestRate;
+   }
+
+   public double getInterestRate() 
+   {
+	   return interestRate;
+   }
+   
+   public double calculateInterest() 
+   {
+	   return balance*interestRate;
+   }
+
+   public void deposit (int money) 
+   {
+	   balance = money+balance;
+   }
+
+   public void withdraw (int money)
+   {
+	   balance = balance-money;
+   }
+}
+
+public class AccountTest {
+      
+   public static void main (String[] args) {
+      
+      account account = new account("411-0290-1203 ",500000,7.3/100); 
+
+
+      
+      System.out.println("계좌정보: "+account.getAccount()+"현재잔액: "+account.getBalance());
+
+      account.deposit(20000); 
+      account.calculateInterest(); 
+      
+      System.out.println("계좌정보: "+ account.getAccount()+"현재잔액: "+account.getBalance());
+      System.out.println("이자: "+account.calculateInterest()); 
+   }
+}
